4dd926da25e7dba5c90f13c55c9d9b12f19e2d0f
Ali Ok <aliok@redhat.com>
2019-05-24 15:21:09 +0300

    AndroidVariant CRD

---
+++ pkg/apis/push/v1alpha1/androidvariant_types.go
@@ -0,0 +1,52 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// AndroidVariantSpec defines the desired state of AndroidVariant
+// +k8s:openapi-gen=true
+type AndroidVariantSpec struct {
+	// Description is human friendly description for the variant.
+	Description string `json:"description,omitempty"`
+	// ServerKey is key from the Firebase Console of a project which has been enabled for FCM.
+	ServerKey string `json:"serverKey"`
+	// SenderId is the "Google Project Number" from the API Console. It is *not* needed for sending push messages, but it is a convenience to "see" it on
+	// the Admin UI as well, since the Android applications require it (called Sender ID there). That way all information is stored on the same object.
+	SenderId string `json:"senderId"`
+}
+
+// AndroidVariantStatus defines the observed state of AndroidVariant
+// +k8s:openapi-gen=true
+type AndroidVariantStatus struct {
+	Ready     bool   `json:"ready"`
+	VariantId string `json:"variantId,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// AndroidVariant is the Schema for the androidvariants API
+// +k8s:openapi-gen=true
+type AndroidVariant struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   AndroidVariantSpec   `json:"spec,omitempty"`
+	Status AndroidVariantStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// AndroidVariantList contains a list of AndroidVariant
+type AndroidVariantList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []AndroidVariant `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&AndroidVariant{}, &AndroidVariantList{})
+}
