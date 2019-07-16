96673993c5e89732de2f703fffcb250a12724c6e
Camila Macedo <camilamacedo86@gmail.com>
2019-07-16 08:49:00 -0300

    feat(UnitTests) : add new unit test

---
+++ pkg/controller/unifiedpushserver/fakeclient_test.go
@@ -0,0 +1,43 @@
+package unifiedpushserver
+
+import (
+	pushv1alpha1 "github.com/aerogear/unifiedpush-operator/pkg/apis/push/v1alpha1"
+	openshiftappsv1 "github.com/openshift/api/apps/v1"
+	routev1 "github.com/openshift/api/route/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/kubernetes/scheme"
+	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+	"testing"
+	imagev1 "github.com/openshift/api/image/v1"
+
+)
+
+//buildReconcileWithFakeClientWithMocks return reconcile with fake client, schemes and mock objects
+func buildReconcileWithFakeClientWithMocks(objs []runtime.Object, t *testing.T) *ReconcileUnifiedPushServer {
+	s := scheme.Scheme
+
+	// Add route Openshift scheme
+	if err := routev1.AddToScheme(s); err != nil {
+		t.Fatalf("Unable to add route scheme: (%v)", err)
+	}
+
+	// Add route Openshift appsv1
+	if err := openshiftappsv1.AddToScheme(s); err != nil {
+		t.Fatalf("Unable to add appsv1 scheme: (%v)", err)
+	}
+
+	// Add route Openshift appsv1
+	if err := imagev1.AddToScheme(s); err != nil {
+		t.Fatalf("Unable to add imagev1 scheme: (%v)", err)
+	}
+
+	s.AddKnownTypes(pushv1alpha1.SchemeGroupVersion, &pushv1alpha1.UnifiedPushServer{})
+	s.AddKnownTypes(pushv1alpha1.SchemeGroupVersion, &pushv1alpha1.UnifiedPushServerList{})
+
+	// create a fake client to mock API calls with the mock objects
+	cl := fake.NewFakeClient(objs...)
+
+	// create a ReconcileMobileSecurityService object with the scheme and fake client
+	return &ReconcileUnifiedPushServer{client: cl, scheme: s}
+}
---
+++ pkg/controller/unifiedpushserver/unifiedpushserver_controller_test.go
@@ -0,0 +1,121 @@
+package unifiedpushserver
+
+import (
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	"testing"
+	"context"
+	openshiftappsv1 "github.com/openshift/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	routev1 "github.com/openshift/api/route/v1"
+	imagev1 "github.com/openshift/api/image/v1"
+
+)
+
+func TestReconcileUnifiedPushServer_Reconcile(t *testing.T) {
+	// objects to track in the fake client
+	objs := []runtime.Object{
+		&pushServerInstance,
+	}
+
+	r := buildReconcileWithFakeClientWithMocks(objs, t)
+
+	// mock request to simulate Reconcile() being called on an event for a watched resource
+	req := reconcile.Request{
+		NamespacedName: types.NamespacedName{
+			Name:      pushServerInstance.Name,
+			Namespace: pushServerInstance.Namespace,
+		},
+	}
+
+	res, err := r.Reconcile(req)
+	if err != nil {
+		t.Fatalf("reconcile: (%v)", err)
+	}
+
+	// Check if persistentVolumeClaim has been created
+	persistentVolumeClaim := &corev1.PersistentVolumeClaim{}
+	err = r.client.Get(context.TODO(), types.NamespacedName{Name: pushServerInstance.Name+"-postgresql", Namespace: pushServerInstance.Namespace}, persistentVolumeClaim)
+	if err != nil {
+		t.Fatalf("get persistentVolumeClaim: (%v)", err)
+	}
+
+	// Check if deployment has been created
+	dep := &openshiftappsv1.DeploymentConfig{}
+	err = r.client.Get(context.TODO(), req.NamespacedName, dep)
+	if err != nil {
+		t.Fatalf("get deployment config: (%v)", err)
+	}
+
+	// Check if service has been created
+	service := &corev1.Service{}
+	err = r.client.Get(context.TODO(), types.NamespacedName{Name: pushServerInstance.Name+"-postgresql", Namespace: pushServerInstance.Namespace}, service)
+	if err != nil {
+		t.Fatalf("get service: (%v)", err)
+	}
+
+	// Check if serviceAccount has been created
+	serviceAccount := &corev1.ServiceAccount{}
+	err = r.client.Get(context.TODO(), types.NamespacedName{Name: pushServerInstance.Name, Namespace: pushServerInstance.Namespace}, serviceAccount)
+	if err != nil {
+		t.Fatalf("get serviceAccount: (%v)", err)
+	}
+
+	// Check if service has been created
+	secret := &corev1.Secret{}
+	err = r.client.Get(context.TODO(), types.NamespacedName{Name: pushServerInstance.Name+"-postgresql", Namespace: pushServerInstance.Namespace}, secret)
+	if err != nil {
+		t.Fatalf("get secret: (%v)", err)
+	}
+
+	// Check if service has been created
+	oauthProxyService := &corev1.Service{}
+	err = r.client.Get(context.TODO(), types.NamespacedName{Name: pushServerInstance.Name+"-unifiedpush-proxy", Namespace: pushServerInstance.Namespace}, oauthProxyService)
+	if err != nil {
+		t.Fatalf("get oauthProxyService: (%v)", err)
+	}
+
+	// Check if service has been created
+	serviceOauth := &corev1.Service{}
+	err = r.client.Get(context.TODO(), types.NamespacedName{Name: pushServerInstance.Name+"-unifiedpush-proxy", Namespace: pushServerInstance.Namespace}, serviceOauth)
+	if err != nil {
+		t.Fatalf("get serviceOauth: (%v)", err)
+	}
+
+	// Check if route has been created
+	serviceOauthRoute := &routev1.Route{}
+	err = r.client.Get(context.TODO(), types.NamespacedName{Name: pushServerInstance.Name+"-unifiedpush-proxy", Namespace: pushServerInstance.Namespace}, serviceOauthRoute)
+	if err != nil {
+		t.Fatalf("get service Oauth route: (%v)", err)
+	}
+
+	// Check if servicePush has been created
+	servicePush := &corev1.Service{}
+	err = r.client.Get(context.TODO(), types.NamespacedName{Name: pushServerInstance.Name+"-unifiedpush", Namespace: pushServerInstance.Namespace}, servicePush)
+	if err != nil {
+		t.Fatalf("get servicePush: (%v)", err)
+	}
+
+	// Check if imageStream has been created
+	oauthImageStream := &imagev1.ImageStream{}
+	err = r.client.Get(context.TODO(), types.NamespacedName{Name: "ups-oauth-proxy-imagestream", Namespace: pushServerInstance.Namespace}, oauthImageStream)
+	if err != nil {
+		t.Fatalf("get oauthImageStream: (%v)", err)
+	}
+
+
+	// Check if imageStream has been created
+	upsImageStream := &imagev1.ImageStream{}
+	err = r.client.Get(context.TODO(), types.NamespacedName{Name: "ups-imagestream", Namespace: pushServerInstance.Namespace}, upsImageStream)
+	if err != nil {
+		t.Fatalf("get upsImageStream: (%v)", err)
+	}
+
+	//TODO:Finish this test
+
+	// Check the result of reconciliation to make sure it has the desired state
+	if res.Requeue {
+		t.Error("reconcile did requeue which is not expected")
+	}
+}
