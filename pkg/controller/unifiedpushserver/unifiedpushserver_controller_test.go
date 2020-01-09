b409931cd27188327f73d5594a748b5d14d8922e
Gerard Ryan <gerard@ryan.lt>
2020-01-09 21:23:37 +0000

    ✅ Clean up reconcile test & prepare for new tests

--- pkg/controller/unifiedpushserver/unifiedpushserver_controller_test.go
+++ pkg/controller/unifiedpushserver/unifiedpushserver_controller_test.go
@@ -2,104 +2,83 @@ package unifiedpushserver
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
+	pushv1alpha1 "github.com/aerogear/unifiedpush-operator/pkg/apis/push/v1alpha1"
+
 	routev1 "github.com/openshift/api/route/v1"
+
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 )
 
 func TestReconcileUnifiedPushServer_Reconcile(t *testing.T) {
-	// objects to track in the fake client
-	objs := []runtime.Object{
-		&pushServerInstance,
-	}
-
-	r := buildReconcileWithFakeClientWithMocks(objs, t)
-
-	// mock request to simulate Reconcile() being called on an event for a watched resource
-	req := reconcile.Request{
-		NamespacedName: types.NamespacedName{
-			Name:      pushServerInstance.Name,
-			Namespace: pushServerInstance.Namespace,
+	scenarios := []struct {
+		name   string
+		given  *pushv1alpha1.UnifiedPushServer
+		expect map[string]runtime.Object
+	}{
+		{
+			name:  "should create expected resources on reconcile of simple empty cr",
+			given: &crWithDefaults,
+			expect: map[string]runtime.Object{
+				crWithDefaults.Name: &appsv1.Deployment{},
+				crWithDefaults.Name: &corev1.ServiceAccount{},
+				fmt.Sprintf("%s-postgresql", crWithDefaults.Name):        &corev1.PersistentVolumeClaim{},
+				fmt.Sprintf("%s-postgresql", crWithDefaults.Name):        &corev1.Service{},
+				fmt.Sprintf("%s-postgresql", crWithDefaults.Name):        &corev1.Secret{},
+				fmt.Sprintf("%s-unifiedpush", crWithDefaults.Name):       &corev1.Service{},
+				fmt.Sprintf("%s-unifiedpush-proxy", crWithDefaults.Name): &corev1.Service{},
+				fmt.Sprintf("%s-unifiedpush-proxy", crWithDefaults.Name): &routev1.Route{},
+			},
 		},
 	}
 
-	res, err := r.Reconcile(req)
-	if err != nil {
-		t.Fatalf("reconcile: (%v)", err)
-	}
+	for _, scenario := range scenarios {
+		t.Run(scenario.name, func(t *testing.T) {
 
-	// Check if persistentVolumeClaim has been created
-	persistentVolumeClaim := &corev1.PersistentVolumeClaim{}
-	err = r.client.Get(context.TODO(), types.NamespacedName{Name: pushServerInstance.Name + "-postgresql", Namespace: pushServerInstance.Namespace}, persistentVolumeClaim)
-	if err != nil {
-		t.Fatalf("get persistentVolumeClaim: (%v)", err)
-	}
+			// given
+			r := buildReconcileWithFakeClientWithMocks([]runtime.Object{scenario.given}, t)
+			req := reconcile.Request{
+				NamespacedName: types.NamespacedName{
+					Name:      scenario.given.Name,
+					Namespace: scenario.given.Namespace,
+				},
+			}
 
-	// Check if deployment has been created
-	dep := &appsv1.Deployment{}
-	err = r.client.Get(context.TODO(), req.NamespacedName, dep)
-	if err != nil {
-		t.Fatalf("get deployment: (%v)", err)
-	}
+			// Required "backupjob" SA for backup CronJobs
+			err := r.client.Create(context.TODO(), &corev1.ServiceAccount{ObjectMeta: metav1.ObjectMeta{Name: "backupjob", Namespace: "unifiedpush"}})
 
-	// Check if service has been created
-	service := &corev1.Service{}
-	err = r.client.Get(context.TODO(), types.NamespacedName{Name: pushServerInstance.Name + "-postgresql", Namespace: pushServerInstance.Namespace}, service)
-	if err != nil {
-		t.Fatalf("get service: (%v)", err)
-	}
+			// when
+			res, err := r.Reconcile(req)
+			if err != nil {
+				t.Fatalf("reconcile: (%v)", err)
+			}
+			if res.Requeue {
+				t.Error("Reconcile requeued unexpectedly")
+			}
 
-	// Check if serviceAccount has been created
-	serviceAccount := &corev1.ServiceAccount{}
-	err = r.client.Get(context.TODO(), types.NamespacedName{Name: pushServerInstance.Name, Namespace: pushServerInstance.Namespace}, serviceAccount)
-	if err != nil {
-		t.Fatalf("get serviceAccount: (%v)", err)
-	}
-
-	// Check if service has been created
-	secret := &corev1.Secret{}
-	err = r.client.Get(context.TODO(), types.NamespacedName{Name: pushServerInstance.Name + "-postgresql", Namespace: pushServerInstance.Namespace}, secret)
-	if err != nil {
-		t.Fatalf("get secret: (%v)", err)
-	}
-
-	// Check if service has been created
-	oauthProxyService := &corev1.Service{}
-	err = r.client.Get(context.TODO(), types.NamespacedName{Name: pushServerInstance.Name + "-unifiedpush-proxy", Namespace: pushServerInstance.Namespace}, oauthProxyService)
-	if err != nil {
-		t.Fatalf("get oauthProxyService: (%v)", err)
-	}
-
-	// Check if service has been created
-	serviceOauth := &corev1.Service{}
-	err = r.client.Get(context.TODO(), types.NamespacedName{Name: pushServerInstance.Name + "-unifiedpush-proxy", Namespace: pushServerInstance.Namespace}, serviceOauth)
-	if err != nil {
-		t.Fatalf("get serviceOauth: (%v)", err)
-	}
-
-	// Check if route has been created
-	serviceOauthRoute := &routev1.Route{}
-	err = r.client.Get(context.TODO(), types.NamespacedName{Name: pushServerInstance.Name + "-unifiedpush-proxy", Namespace: pushServerInstance.Namespace}, serviceOauthRoute)
-	if err != nil {
-		t.Fatalf("get service Oauth route: (%v)", err)
-	}
-
-	// Check if servicePush has been created
-	servicePush := &corev1.Service{}
-	err = r.client.Get(context.TODO(), types.NamespacedName{Name: pushServerInstance.Name + "-unifiedpush", Namespace: pushServerInstance.Namespace}, servicePush)
-	if err != nil {
-		t.Fatalf("get servicePush: (%v)", err)
-	}
-
-	//TODO:Finish this test
-
-	// Check the result of reconciliation to make sure it has the desired state
-	if res.Requeue {
-		t.Error("reconcile did requeue which is not expected")
+			// then
+			for n, o := range scenario.expect {
+				err = r.client.Get(context.TODO(), types.NamespacedName{Name: n, Namespace: scenario.given.Namespace}, o)
+				if err != nil {
+					t.Fatalf("get %s %s: (%v)", o.GetObjectKind(), n, err)
+				}
+			}
+		})
 	}
 }
+
+var (
+	crWithDefaults = pushv1alpha1.UnifiedPushServer{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "example-unifiedpushserver",
+			Namespace: "unifiedpush",
+		},
+	}
+)
